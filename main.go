26e6b2af8aacc00de4940fec7d03523cb6a773a9
Иван <games10loh@MacBook-Air.local>
2026-09-13 00:17:56 +0300

    minor changes

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func twoSum(nums []int, target int) []int {
-	for i := 0; i < len(nums)-1; i++ {
+	for i := 0; i < len(nums) - 1; i++ {
 		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
+			if nums[i] + nums[j] == target {
 				return []int{i, j}
 			}
 		}
@@ -14,5 +14,5 @@ func twoSum(nums []int, target int) []int {
 }
 
 func main() {
-	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+	fmt.Println(twoSum([]int{2, 6, 7, 15}, 9))
 }
