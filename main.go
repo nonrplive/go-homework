2862edd562d1e836c15d942073078a1d38aad1dd
Иван <games10loh@MacBook-Air.local>
2026-09-13 00:24:51 +0300

    homework 1: add header comment

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
+// Homework 1: Two Sum solution
+
 package main
 
 import "fmt"
 
 func twoSum(nums []int, target int) []int {
-	for i := 0; i < len(nums) - 1; i++ {
+	for i := 0; i < len(nums)-1; i++ {
 		for j := i + 1; j < len(nums); j++ {
-			if nums[i] + nums[j] == target {
+			if nums[i]+nums[j] == target {
 				return []int{i, j}
 			}
 		}
